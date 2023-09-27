ef78efad1f3750da2fbb8b227aaeb6961b8b9ca5
Lee-nkoana <lebonkoana6@gmail.com>
2023-09-27 08:25:53 +0200

    Basic calculator functions added

---
+++ script.js
@@ -0,0 +1,27 @@
+let num1 = 0;
+let num2 = 0;
+let operator
+
+function add(x,y){
+    return x + y
+}
+
+console.log(add(4,4));
+
+function subtract(x,y){
+    return x - y
+}
+
+console.log(subtract(4,1));
+
+function divide(x,y){
+    return x / y
+}
+
+console.log(divide(4,2));
+
+function multiply(x,y){
+    return x * y
+}
+
+console.log(multiply(5,2));
